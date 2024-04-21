0752d521bb39070da88778b1fc4b74dc34e0e73c
jatin gyass <jatingyass9@gmail.com>
2024-04-21 14:01:54 +0530

    DP catalan base question

---
+++ DP/MountainRanges.java
@@ -0,0 +1,26 @@
+public class MountainRanges {
+    // TC = O(n*n)
+    public static int mountainRanges(int n){
+        int dp[] = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<n+1; i++){
+            //i pairs -> mountain ranges => ci
+            for(int j=0; j<i; j++){
+                int inside = dp[j];
+                int outside = dp[i-j-1];
+                dp[i] += inside*outside; //ci = cj * ci-j-1
+            }
+        }
+         //n pairs
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println(mountainRanges(n));
+    }
+    
+}
